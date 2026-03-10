4847a20aa8d411674f644fe6a5c3ffaad162b1ea
Akshat <akshanik1104@gmail.com>
2026-03-10 18:40:15 +0400

    Start Screen update

--- Scripts/start_button.gd
+++ Scripts/start_button.gd
@@ -1,5 +1,7 @@
 extends Control
 
+signal start_pressed
+
 @onready var start_button: TextureButton = $StartButton
 @onready var label: Label = $StartButton/Label
 @onready var hover_sound: AudioStreamPlayer = $HoverSound
@@ -8,8 +10,8 @@ func _ready() -> void:
 	if start_button:
 		start_button.mouse_entered.connect(_on_start_button_mouse_entered)
 		start_button.mouse_exited.connect(_on_start_button_mouse_exited)
+		start_button.pressed.connect(_on_start_button_pressed)
 
-	# Ensure initial (non-hovered) state uses black text.
 	if label:
 		label.add_theme_color_override("font_color", Color.BLACK)
 
@@ -23,4 +25,15 @@ func _on_start_button_mouse_entered() -> void:
 func _on_start_button_mouse_exited() -> void:
 	if label:
 		label.add_theme_color_override("font_color", Color.BLACK)
+
+func _on_start_button_pressed() -> void:
+	# Fade this whole control out, then notify listeners to start the game.
+	var tween := create_tween()
+	tween.tween_property(self, "modulate:a", 0.0, 0.4)
+	tween.finished.connect(_on_fade_finished)
+
+func _on_fade_finished() -> void:
+	hide()
+	start_pressed.emit()
+
 
